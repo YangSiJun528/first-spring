ff0f06add8ede0a6812c87ea2cedca236239241a
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-11-29 16:14:43 +0900

    Initial commit

---
+++ src/main/java/com/example/first/controller/ApiController.java
@@ -0,0 +1,9 @@
+package com.example.first.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // RestApi 처리하는 Controller
+@RequestMapping("/api") //RequestMapping 지정
+public class ApiController {
+}
